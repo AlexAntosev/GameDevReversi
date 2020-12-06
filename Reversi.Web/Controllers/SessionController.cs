2b29a6d52b16d9a18cab747bac97302efedf2beb
Oleksandr Antosiev <oleksandr.antosiev@wolterskluwer.com>
2020-12-06 19:15:47 +0200

    Add session controller

---
+++ Reversi.Web/Controllers/SessionController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Reversi.Business.Contracts.Enums;
+using Reversi.Business.Contracts.Models;
+using Reversi.Business.Contracts.Services;
+
+namespace Reversi.Web.Controllers
+{
+    [Route("api/session")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        private readonly ISessionService _sessionService;
+
+        public SessionController(ISessionService sessionService)
+        {
+            _sessionService = sessionService;
+        }
+        
+        [HttpPost]
+        public void CreateNew()
+        {
+            _sessionService.CreateNewSession(new Player(Color.Light), new Player(Color.Dark));
+        }
+    }
+}
